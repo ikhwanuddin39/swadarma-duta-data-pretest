262038f579d260d66ec4eb0c411f121bd3148fd4
ikhwanuddin39 <ikhwanuddin039@gmail.com>
2023-10-03 13:38:38 +0700

    add: list data module

---
+++ src/app/views/dashboard/list-data/list-data.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListDataComponent } from './list-data.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListDataComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ListDataModule { }
